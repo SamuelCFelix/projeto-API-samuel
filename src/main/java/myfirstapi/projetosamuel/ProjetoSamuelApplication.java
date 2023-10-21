adeca453e888a3322843c34a5036a8ceec2db8c6
samuel felix <samuelfnatal@hotmail.com>
2023-10-21 14:53:09 -0300

    Initial commit

---
+++ src/main/java/myfirstapi/projetosamuel/ProjetoSamuelApplication.java
@@ -0,0 +1,13 @@
+package myfirstapi.projetosamuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSamuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSamuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myfirstapi/projetosamuel/ProjetoSamuelApplicationTests.java
@@ -0,0 +1,13 @@
+package myfirstapi.projetosamuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSamuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
